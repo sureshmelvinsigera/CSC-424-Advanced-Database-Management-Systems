a37b57b41d6bcf16ced7287f599591008006fed8
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2019-01-31 10:00:02 -0500

    Initial commit

---
+++ week2/list-loop-part1.2.py
@@ -0,0 +1,4 @@
+grades = [100, 20, 90, 100, 89]
+
+for i in range(len(grades)):
+    print(grades[i])
---
+++ week2/list-loop-part1.3.py
@@ -0,0 +1,4 @@
+grades = [100, 20, 90, 100, 89]
+
+for pos, value in enumerate(grades):
+    print(pos, ' ', value)
