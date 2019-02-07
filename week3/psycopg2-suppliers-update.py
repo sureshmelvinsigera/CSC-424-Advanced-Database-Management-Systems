7f33567cda642bcb14182bd6746590692e83e8c6
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2019-02-07 17:16:21 -0500

    Initial commit

---
+++ week3/psycopg2-suppliers-update.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+
+def update_vendor(vendor_id, vendor_name):
+    """ update vendor name based on the vendor id """
+    sql = """ UPDATE vendors
+                SET vendor_name = %s
+                WHERE vendor_id = %s"""
+    conn = None
+    updated_rows = 0
+    try:
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(user="postgres",
+                                password="CSI1",
+                                host="localhost",
+                                port="5432",
+                                database="suppliers")
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the UPDATE  statement
+        cur.execute(sql, (vendor_name, vendor_id))
+        # get the number of updated rows
+        updated_rows = cur.rowcount
+        # Commit the changes to the database
+        conn.commit()
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return updated_rows
+
+
+if __name__ == '__main__':
+    update_vendor(1, "3M Inc.")
