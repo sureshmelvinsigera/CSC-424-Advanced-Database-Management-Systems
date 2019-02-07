8244c6e02a7018dd2dc983f99d25debdb1c850ec
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2019-02-07 16:43:57 -0500

    Initial commit

---
+++ week3/psycopg2-suppliers-insert-one-row.py
@@ -0,0 +1,37 @@
+import psycopg2
+
+
+def insert_vendor(vendor_name):
+    """ insert a new vendor into the vendors table """
+    sql = """INSERT INTO vendors(vendor_name)
+             VALUES(%s) RETURNING vendor_id;"""
+    conn = None
+    vendor_id = None
+    try:
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(user="postgres",
+                                password="CSI1",
+                                host="localhost",
+                                port="5432",
+                                database="suppliers")
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        cur.execute(sql, (vendor_name,))
+        # get the generated id back
+        vendor_id = cur.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return vendor_id
+
+
+if __name__ == '__main__':
+    insert_vendor("3M Co.")
